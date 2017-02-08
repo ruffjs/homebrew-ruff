60e4fe2c750c57da1ba17a07708d3c5c27d55c99
rppro <rppro@rppro.com>
2017-02-08 16:11:37 +0800

    release for 1.7.0

--- Formula/ruff.rb
+++ Formula/ruff.rb
@@ -1,8 +1,8 @@
 class Ruff < Formula
   desc "IoT platform with JavaScript as application language"
   homepage "https://ruff.io"
-  url "http://sdk.ruff.io/ruff-sdk-mac-1.6.2.zip"
-  sha256 "e5e58a752aa8cdf4e924ce7946e3a26a175cf5b75519849cb52b742689fe33d6"
+  url "https://link.ruff.io/?target=ruff-sdk-mac-1.7.0.zip"
+  sha256 "7e8ae1dff14c067cb4c5a7f80de2378603b5e2eb7f2ebb5afd5af6246ff48bc7"
 
   def install
     prefix.install Dir["*"]
