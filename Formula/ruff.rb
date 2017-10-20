0a855503ce47cd6ca8c8e2913d98cebde7a2e622
NuisTNeO <spacestar_3@hotmail.com>
2017-10-20 10:35:46 +0800

    fix bug: don`t match between file and SHA

--- Formula/ruff.rb
+++ Formula/ruff.rb
@@ -2,7 +2,7 @@ class Ruff < Formula
   desc "IoT platform with JavaScript as application language"
   homepage "https://ruff.io"
   url "https://link.ruff.io/?target=ruff-sdk-mac-1.11.1.zip"
-  sha256 "a15259594d5176fa7c1b6c8c16b99505c52f0bb0930cfdbeaaec9bddf5efb089"
+  sha256 "41fe16153334cd6cc1da011ba1c78dd6325f7a2816d1fbeee669ce82a3366c0a"
 
   def install
     prefix.install Dir["*"]
