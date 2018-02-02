516b46230f0ff01ec35094aebd77aaa7a9aaccfc
Young <young.mu@aliyun.com>
2018-02-02 13:58:05 +0800

    Update for v1.11.5

--- Formula/ruff.rb
+++ Formula/ruff.rb
@@ -1,8 +1,8 @@
 class Ruff < Formula
   desc "IoT platform with JavaScript as application language"
   homepage "https://ruff.io"
-  url "https://link.ruff.io/?target=ruff-sdk-mac-1.11.4.zip"
-  sha256 "852d91fd0de7c62a334a914f54e30480c0f20fd4c32aa5495f3586a488534c1a"
+  url "https://link.ruff.io/?target=ruff-sdk-mac-1.11.5.zip"
+  sha256 "8d781fe86b17f17f29d387c6018164b1e34dd33c304f90718c4ed26408e4db9d"
 
   def install
     prefix.install Dir["*"]
